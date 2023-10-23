6d175bb6fb476c08cfb66632d246119f87e35bb7
ilyastar9999 <66072408+ilyastar9999@users.noreply.github.com>
2023-10-23 16:17:47 +0300

    Update db_code.py

---
+++ db_code.py
@@ -0,0 +1,76 @@
+import psycopg2
+import json
+#import sqlite3
+
+config_file = open("config.json")
+config_data = json.load(config_file)
+host = config_data["db_host"]
+port = config_data["db_port"]
+user = config_data["db_user"]
+database = config_data["db_name"]
+password = config_data["db_pass"]
+
+conn = psycopg2.connect(
+    host=host,
+    port=port,
+    database=database,
+    user=user,
+    password=password
+)
+
+#conn = sqlite3.connect('db.sql')
+
+cursor = conn.cursor()
+
+conn.rollback()
+
+def parse_data(field):
+    file = open("config.json")
+    data = json.load(file)[field]
+
+    return data
+
+def create_user(name, password, email):
+    try:
+        cursor.execute("""INSERT INTO users (name, password, auth, email) VALUES (%s, %s, %s, %s)""", (name, password, False, email))
+        conn.commit()
+        return True
+    except:
+        return False
+
+def check_auth_user(username):
+    sqlite3_select_query = """SELECT auth FROM users WHERE email = %s;"""
+    cursor.execute(sqlite3_select_query, (username, ))
+    conn.commit()
+    try:
+        if cursor.fetchall()[0][0]:
+            return True
+        else:
+            return False
+    except:
+        return True
+
+def auth_user(username):
+    sqlite3_select_query = """Update users SET auth = true WHERE email = %s;"""
+    cursor.execute(sqlite3_select_query, (username, ))
+    return
+
+def check_not_auth_user_is_exist(username):
+    sqlite3_select_query = """SELECT email FROM users WHERE email =%s;"""
+    cursor.execute(sqlite3_select_query, (username, ))
+    conn.commit()
+    return cursor.fetchall()
+
+def create_all():
+    sqlite_select_query = ["""CREATE TABLE IF NOT EXISTS marks(id SERIAL PRIMARY KEY, value INTEGER, user_id SEREAL)""", 
+"""CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, name TEXT, password TEXT, auth BOOLEAN, email TEXT UNIQUE);""",
+"""CREATE TABLE IF NOT EXISTS classes(id SERIAL PRIMARY KEY, name TEXT, members );"""]
+    cursor.execute(sqlite_select_query[0])
+    cursor.execute(sqlite_select_query[1])
+    conn.commit() 
+    try:
+        create_user("admin", "silaederprojects@gmail.com", parse_data("secret_key"), "Admin", "Adminovich")
+        auth_user('admin')
+    except:
+        pass
+    return
